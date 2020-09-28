d941c776d37d51c29f65275366ed87988b15d2b0
Merkurev Petr <9662196@mail.ru>
2020-09-28 20:09:03 +0300

    first commit

---
+++ src/main/java/DecrypterThread.java
@@ -0,0 +1,72 @@
+import net.lingala.zip4j.core.ZipFile;
+import net.lingala.zip4j.exception.ZipException;
+
+import java.io.File;
+
+public class DecrypterThread extends Thread
+{
+    private GUIForm form;
+    private File file;
+    private String password;
+
+    public DecrypterThread(GUIForm form)
+    {
+        this.form = form;
+    }
+
+    public void setFile(File file) {
+        this.file = file;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    @Override
+    public void run()
+    {
+        onStart();
+        try
+        {
+            String outPath = getOutputPath();
+            ZipFile zipFile = new ZipFile(file);
+            zipFile.setPassword(password);
+            zipFile.extractAll(outPath);
+        }
+        catch (ZipException ex) {
+            form.showWarning("Неверный пароль. Попробуйте еще");
+
+        }
+        catch (Exception ex) {
+            form.showWarning(ex.getMessage());
+
+        }
+        onFinish();
+    }
+
+    private void onStart()
+    {
+        form.setButtonsEnabled(false);
+    }
+
+    private void onFinish()
+    {
+        form.setButtonsEnabled(true);
+        form.showFinished();
+    }
+
+    private String getOutputPath()
+    {
+        String path = file.getAbsolutePath()
+                .replaceAll("\\.enc$", "");
+        for(int i = 1; ; i++)
+        {
+            String number = i > 1 ? Integer.toString(i) : "";
+            String outPath = path + number;
+            if(!new File(outPath).exists()) {
+                return outPath;
+            }
+        }
+    }
+}
